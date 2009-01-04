74f7557798d37f14bbb34766185bafe4c3edc856
Dima Berastau <dima@ruboss.com>
2009-01-03 18:49:15 -0800

    Applied a fix for main.py generator.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import wsgiref.handlers
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-from app.controllers import locations, notes, projects, restful, tasks, users
+from app.controllers import locations, notes, projects, tasks, users
 
 class AppController(webapp.RequestHandler):
   def get(self):
@@ -18,7 +18,6 @@ def main():
   ('/locations.*', locations.Controller),
   ('/notes.*', notes.Controller),
   ('/projects.*', projects.Controller),
-  ('/restful.*', restful.Controller),
   ('/tasks.*', tasks.Controller),
   ('/users.*', users.Controller)
   ], debug=True)
